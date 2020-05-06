d86f7648280fae243458dac7d22f0b2cc62e66c9
MarcusKyleThompson <63977697+MarcusKyleThompson@users.noreply.github.com>
2020-05-06 17:36:22 -0500

    Plotted data

--- pandas data import.py
+++ pandas data import.py
@@ -8,7 +8,7 @@ Created on Wed Apr 29 22:31:15 2020
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 df = pd.read_csv('flightdata.txt', delim_whitespace=True)
 
@@ -16,10 +16,16 @@ print(df.columns)
 
 x = (df['<Clock>[ms]'])
 y = (df['<Height>[m]'])
+pitch = (df['<Pitch>[rad]'])
+
 
 print(x,y)
 
+plt.plot(x,y)
+plt.show()
 
+plt.plot(x,pitch)
+plt.show()
 # <Clock>[ms] x-axis
 # <Height>[m] y-axis
 
